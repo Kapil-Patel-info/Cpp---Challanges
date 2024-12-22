5eb8eb8cad5cd04f6d33406195608a7ca7bc644f
Thekapilpatel <TheKapilpatelofficial@gmail.com>
2024-12-22 15:44:02 +0530

    armstrong number commit

---
+++ Armstrong.cpp
@@ -0,0 +1,38 @@
+
+/* Armstrong Number Explanation
+            An Armstrong
+            number(also known as a narcissistic number, pluperfect number, or pluperfect digital invariant) is a number that is equal to the sum of its own digits raised to the power of the number of digits.
+
+
+        
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    int sum = 0;
+    int temp = n;
+
+    while (temp)
+    {
+        int last_digit = temp % 10;
+        sum += (last_digit * last_digit * last_digit);
+        temp = temp / 10;
+    }
+
+    if (sum == n)
+    {
+        cout << n << " is an Armstrong number";
+    }
+    else
+    {
+        cout << n << " is not an Armstrong number";
+    }
+
+    return 0;
+}
