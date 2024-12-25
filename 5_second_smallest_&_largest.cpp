3b7cdbdcddc3c051551695e13146378a5a3fe406
Thekapilpatel <TheKapilpatelofficial@gmail.com>
2024-12-25 11:49:05 +0530

    day 4

---
+++ 5_second_smallest_&_largest.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int arr[] = {9, 8, 2, 3, 4, 5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout<<"testing";
+
+    // for (int i = 0; i <= 4; i++)
+    // {
+    //     cout << arr[i];
+    // }
+
+    return 0;
+}
