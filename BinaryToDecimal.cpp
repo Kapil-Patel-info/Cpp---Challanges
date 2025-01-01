ecbf88fabc68d6efc5b03af94ad10b8220dc2d7e
Thekapilpatel <TheKapilpatelofficial@gmail.com>
2025-01-01 18:06:18 +0530

    que 10th

---
+++ BinaryToDecimal.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std; 
+
+int BinaryToDecimal(int Binary){
+
+int power = 1; int ans =0;
+
+while(Binary){
+
+int lastdigit = Binary%10;
+
+ ans += lastdigit*power;
+
+Binary /= 10;
+ 
+power *= 2;
+
+
+}
+return ans;
+}
+
+int main(){
+
+cout<<BinaryToDecimal(111);
+
+}
